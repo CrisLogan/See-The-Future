3d493805a0fcecb360e8895d672d46d82ee41889
CrisLogan <Cristopherlogan18@gmail.com>
2025-08-04 11:19:53 -0500

    Primer commit con la web completa

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("logoClick").addEventListener("click", function () {
+    document.getElementById("introScreen").classList.add("hidden");
+    document.getElementById("mainContent").classList.remove("hidden");
+});
